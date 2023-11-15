2c94ed2e03fa841cb2cefec1c005298e3d231a33
halyna <1208petrova@gmail.com>
2023-11-15 17:51:42 +0200

    created user/urls.py file

---
+++ user/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path, include
+from rest_framework import routers
+from user.views import (
+    UserListView,
+    UserDetailView,
+    UserCreateView,
+    UserUpdateView,
+    UserDeleteView,
+    UserFollow,
+    ManageUserView,
+)
+
+router = routers.DefaultRouter()
+
+
+urlpatterns = [
+    path("", UserListView.as_view(), name="user-list"),
+    path("", include(router.urls)),
+    path("<int:pk>/", UserDetailView.as_view(), name="user-detail"),
+    path("register/", UserCreateView.as_view(), name="user-register"),
+    path("<int:pk>/update/", UserUpdateView.as_view(), name="user-update"),
+    path("<int:pk>/delete/", UserDeleteView.as_view(), name="user-delete"),
+    path("follow/<int:pk>/", UserFollow.as_view(), name="user-follow"),
+    path("me/", ManageUserView.as_view(), name="manage"),
+]
+app_name = "user"
