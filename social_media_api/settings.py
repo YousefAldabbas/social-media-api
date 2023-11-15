f6710c2b40e7834b589801c22941ed4d5d50d9a6
halyna <1208petrova@gmail.com>
2023-11-15 12:07:45 +0200

    added users models: User, UserFollowing, Location, UserManager

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -113,11 +113,16 @@ USE_I18N = True
 
 USE_TZ = False
 
+AUTH_USER_MODEL = "user.User"
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- user/models.py
+++ user/models.py
@@ -1,3 +1,91 @@
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.utils.translation import gettext as _
 from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("The given email must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class Location(models.Model):
+    country = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+
+
+class User(AbstractUser):
+    avatar = models.ImageField(
+        null=True,
+        blank=True,
+        upload_to="images/avatar/",
+    )
+    lives_in = models. ForeignKey(
+        Location,
+        on_delete=models.CASCADE,
+        related_name="users",
+        null=True,
+        blank=True,
+    )
+    date_of_birth = models.DateField(
+        null=True,
+        blank=True,
+    )
+    bio = models.TextField(
+        null=True,
+        blank=True,
+    )
+    date_of_joining = models.DateTimeField(
+        auto_now_add=True,
+    )
+    username = None
+    email = models.EmailField(
+        _("email address"),
+        unique=True,
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+
+class UserFollowing(models.Model):
+    user_id = models.ForeignKey(
+        User,
+        related_name="users",
+        on_delete=models.CASCADE,
+    )
+    user_following = models.ForeignKey(
+        User,
+        related_name="users",
+        on_delete=models.CASCADE,
+    )
+
+    class Meta:
+        unique_together = (("user_id", "following_user_id"),)
+        index_together = (("user_id", "user_following"),)
+        ordering = ["user.first_name"]
