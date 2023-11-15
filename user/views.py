de6edcf6bf5e2a05a51f08bc31bb229d756777f7
halyna <1208petrova@gmail.com>
2023-11-15 17:50:45 +0200

    created views for users endpoints

--- user/views.py
+++ user/views.py
@@ -1,3 +1,95 @@
-from django.shortcuts import render
+from django.db.models import Count
+from django.http import Http404
+from rest_framework import generics, viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from user.models import User, UserFollowing
+from user.serializers import (
+    UserSerializer,
+    UserListSerializer,
+    UserDetailSerializer,
+    UserFollowingSerializer,
+)
+
+
+class UserListView(generics.ListAPIView):
+    serializer_class = UserListSerializer
+    queryset = User.objects.all()
+
+    def get_queryset(self):
+        queryset = self.queryset.annotate(
+                num_following=Count("following"),
+                num_followers=Count("followers"),
+            )
+
+        return queryset
+
+    def get_object(self):
+        return self.request.user
+
+
+class UserDetailView(generics.RetrieveAPIView):
+    serializer_class = UserDetailSerializer
+    queryset = User.objects.all()
+
+
+class UserCreateView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserUpdateView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserDeleteView(generics.RetrieveDestroyAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserFollowingView(viewsets.ModelViewSet):
+    serializer_class = UserFollowingSerializer
+    queryset = UserFollowing.objects.all()
+
+
+class UserFollow(APIView):
+    def get_object(self, pk):
+        try:
+            return User.objects.get(pk=pk)
+        except User.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        user = self.get_object(pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+    def post(self, request, pk):
+        user = request.user
+        follow = self.get_object(pk)
+        UserFollowing.objects.create(
+            user_id=user,
+            user_following=follow
+        )
+        serializer = UserSerializer(follow)
+        return Response(serializer.data)
+
+    def delete(self, request, pk):
+        user = request.user
+        follow = self.get_object(pk)
+        connection = UserFollowing.objects.filter(
+            user_id=user,
+            user_following=follow
+        ).first()
+        connection.delete()
+        serializer = UserSerializer(follow)
+        return Response(serializer.data)
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
