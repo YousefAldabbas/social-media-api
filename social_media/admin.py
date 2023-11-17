bd2cb5d285d999061cae00b580c3087fb0366425
halyna <1208petrova@gmail.com>
2023-11-17 11:53:39 +0200

    created admin functionality for social_media models

--- social_media/admin.py
+++ social_media/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from social_media.models import Category, Post, Comment, Like
+
+admin.site.register(Category)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Like)
